b125a8924e15a369a6341cb0b7a4edc70af563b6
Sriram <sriramsk1999@gmail.com>
2021-03-04 23:26:40 +0530

    Updated API endpoint

--- lib/utilities/api_service.dart
+++ lib/utilities/api_service.dart
@@ -4,7 +4,7 @@ import 'package:frontend_gaas/utilities/login_model.dart';
 
 class APIService {
   Future<LoginResponseModel> login(LoginRequestModel requestModel) async {
-    String url = "http://127.0.0.1:5000/login";
+    String url = "https://54.175.183.221:5000/login";
 
     final response = await http.post(url, body: requestModel.toJson());
     if (response.statusCode == 200 || response.statusCode == 400) {
